85fcacd4c98befd98ddb82c801973775be1c3441
Kadir AK <kadir.akx@gmail.com>
2025-01-09 22:14:37 +0300

    Proje dosyası ekle.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Dynotis_Calibration_and_Signal_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynotis_Calibration_and_Signal_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Itki_DataList_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+
+        private void Itki_DataList_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void Tork_DataList_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+
+        private void Tork_DataList_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void Cross_DataList_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+
+        private void Cross_DataList_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void Akım_DataList_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+
+        private void Akım_DataList_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void Voltaj_DataList_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+
+        private void Voltaj_DataList_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Itki_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Itki_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Tork_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Tork_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Tork_Mesafesi_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Tork_Mesafesi_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Akım_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Akım_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Voltaj_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Uygulanan_Voltaj_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Calibration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExcelExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExcelImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddPointButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeletePointButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Port_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PlotButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Itki_Sonuc_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tork_Sonuc_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tablolar_TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
